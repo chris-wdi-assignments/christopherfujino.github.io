90d0a65b768bc1b31d0fd4e1d51a02005ef4fd99
Christopher Fujino <christopherfujino@gmail.com>
2017-06-27 18:31:31 -0700

    changed: significant styling changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,11 @@
 // sanity check
 console.log("app.js linked.");
+
+window.onload = function () {
+  var projects = document.querySelectorAll('#portfolio-list > li');
+  projects.forEach(function (el) {
+    el.addEventListener('click', function (e) {
+      window.open(this.getAttribute('data-href'), '_blank');
+    });
+  })
+};
